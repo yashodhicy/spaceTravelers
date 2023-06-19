1bdf933a3ce6323cad7a1b94b9f078a03bff293f
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-19 22:51:09 +0500

    Create basic structure for rockets, add empty nav and empty My Profile.

---
+++ src/components/MyProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyProfile = () => (
+  <>
+    <h1>I am Profile Component.</h1>
+  </>
+);
+
+export default MyProfile;
---
+++ src/components/Rockets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Rockets = () => (
+  <>
+    <h1>I am Rockets Component.</h1>
+  </>
+);
+
+export default Rockets;
