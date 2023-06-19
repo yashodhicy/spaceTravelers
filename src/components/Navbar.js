8c18cc16eda8333e8dc1dee7bb3d068b42d95189
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-19 23:02:37 +0500

    Fix Linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Navbar = () => (
   <>
     <h1>I am Navbar Component.</h1>
     <ul>
-        <li><Link to="/">Rockets</Link></li>
-        <li><Link to="/myprofile">My Profile</Link></li>
+      <li><Link to="/">Rockets</Link></li>
+      <li><Link to="/myprofile">My Profile</Link></li>
     </ul>
   </>
 );
