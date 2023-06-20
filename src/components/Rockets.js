d94b73f168aa2ba7e740e228395458f94a82f264
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-21 04:05:38 +0500

    Fetch and render rockets data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { FetchRockets } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <>
-    <h1>I am Rockets Component.</h1>
-  </>
-);
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rocketsLoaded = useSelector((state) => state.rockets.rockets.length > 0);
+  useEffect(() => {
+    if (!rocketsLoaded) {
+      dispatch(FetchRockets());
+    }
+  }, [dispatch, rocketsLoaded]);
+
+  const allRockets = useSelector((state) => state.rockets.rockets);
+
+  return (
+    <div className="mx-5">
+      <ul className="rockets">
+        {allRockets.map((rocket) => (
+          <li key={rocket.id} className="rocket">
+            <img alt="rocket" className="rocket-image" src={rocket.image} style={{ width: 300 }} />
+            <div className="rocket-details">
+              <h2>{rocket.rocket_name}</h2>
+              <p>{rocket.description}</p>
+              <button type="button" className="btn btn-primary">
+                Reserve Rocket
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
