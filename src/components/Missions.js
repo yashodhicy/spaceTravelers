f51afd8ca2a9ec6d6fae3352989d436af1dac07c
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-20 16:23:18 +0300

    fetch missions data

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { FetchMissions } from '../redux/missions/missionsSlice';
 
-const Missions = () => (
-  <>
-    <h1>I am Missions Component.</h1>
-  </>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, []);
+
+  return (
+    <>
+      <h1>I am Missions Component.</h1>
+    </>
+  );
+};
 
 export default Missions;
