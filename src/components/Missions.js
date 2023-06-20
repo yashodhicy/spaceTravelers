4383ba6e03c82e18367d8834c71154e085f25c7d
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-20 20:05:43 +0300

    add styles to mission and my profile

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,42 +1,46 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
 import { FetchMissions } from '../redux/missions/missionsSlice';
 
 const Missions = () => {
   const dispatch = useDispatch();
+  const missionsLoaded = useSelector((state) => state.missions.missions.length > 0);
   useEffect(() => {
-    dispatch(FetchMissions());
-  }, [dispatch]);
+    if (!missionsLoaded) {
+      dispatch(FetchMissions());
+    }
+  }, [dispatch, missionsLoaded]);
 
   const allMissons = useSelector((state) => state.missions.missions);
 
   return (
-    <>
-      <table className="missions-table">
-        <thead>
-          <tr>
-            <td>Mission</td>
-            <td>discription</td>
-            <td>status</td>
-            <td>
-              {' '}
-              {'   '}
-              {' '}
-            </td>
+    <div className="mx-5">
+    <Table striped bordered hover responsive="sm">
+      <thead>
+        <tr className="fw-bold">
+          <td  style={{ width: '10%' }}>Mission</td>
+          <td style={{ width: '70%' }}>discription</td>
+          <td style={{ width: '10%' }}>status</td>
+          <td style={{ width: '10%' }}>
+            {' '}
+            {'   '}
+            {' '}
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        {allMissons.map((el) => (
+          <tr key={el.mission_id}>
+            <td className="fw-bold">{el.mission_name}</td>
+            <td>{el.description}</td>
+            <td>  tobe</td>
+            <td>  tobe</td>
           </tr>
-        </thead>
-        <tbody>
-          {allMissons.map((el) => (
-            <tr key={el.mission_id}>
-              <td>{el.mission_name}</td>
-              <td>{el.description}</td>
-              <td>  tobe</td>
-              <td>  tobe</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </Table>
+    </div>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Stack from 'react-bootstrap/Stack';
+import Uranus from './assets/uranus.png';
 
 const Navbar = () => (
-  <>
-    <h1>I am Navbar Component.</h1>
-    <ul>
-      <li><Link to="/">Rockets</Link></li>
-      <li><Link to="/missions">Missions</Link></li>
-      <li><Link to="/myprofile">My Profile</Link></li>
-    </ul>
-  </>
+  <Stack direction="horizontal" gap={3} className='mx-5 my-3 pb-3' style={{ borderBottom: '1px solid black' }} >
+    <img src={Uranus} style={{ width: 64, height: 64 }} alt='logo'/>
+    <h1> Space Travelers' Hub</h1>
+    <Nav activeKey="/" className="ms-auto">
+      <Nav.Item><Nav.Link as={NavLink} to="/">Rockets</Nav.Link></Nav.Item>
+      <Nav.Item><Nav.Link as={NavLink} to="/missions" style={{ borderRight: '2px solid black', paddingRight: '15px' }}>Missions</Nav.Link></Nav.Item>
+      <Nav.Item><Nav.Link as={NavLink} to="/myprofile">My Profile</Nav.Link></Nav.Item>
+    </Nav>
+  </Stack>
 );
 
 export default Navbar;
