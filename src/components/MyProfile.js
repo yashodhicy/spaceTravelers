f9df21922ed45bf17cb995fc5264077fb538d0ae
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-22 03:21:42 +0500

    Display reserved rockets in my profile

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -3,23 +3,42 @@ import { Table } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
   const missions = useSelector((state) => state.missions.missions);
   const joinedmissions = missions.filter((mission) => mission.reserved === true);
   return (
-    <div className="mx-5 px-4" style={{ width: '50%' }}>
-      <h2>My Missions</h2>
-      <Table bordered>
-        <tbody>
-          {
+    <div className="profile-container mx-5">
+      <div className="px-4" style={{ width: '50%' }}>
+        <h2>My Missions</h2>
+        <Table bordered>
+          <tbody>
+            {
         joinedmissions.map((mission) => (
           <tr key={mission.mission_id}>
             <td style={{ fontWeight: '400' }}>{mission.mission_name}</td>
           </tr>
         ))
 }
-        </tbody>
+          </tbody>
 
-      </Table>
+        </Table>
+      </div>
+      <div className="px-4" style={{ width: '50%' }}>
+        <h2>My Rockets</h2>
+        <Table bordered>
+          <tbody>
+            {
+            reservedRockets.map((rocket) => (
+              <tr key={rocket.id}>
+                <td style={{ fontWeight: '400' }}>{rocket.rocket_name}</td>
+              </tr>
+            ))
+}
+          </tbody>
+
+        </Table>
+      </div>
     </div>
   );
 };
